1bc25e33fb9cba00ea720ff1ea38822b4c3151a6
Solar-Plexus <juan062494@gmail.com>
2017-05-01 15:00:06 -0700

    Initial commit added all files

---
+++ public/controllers/controller.js
@@ -0,0 +1,58 @@
+var myApp = angular.module('myApp', []);
+myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+    console.log("Hello World from controller");
+
+
+var refresh = function() {
+  $http.get('/todolist').success(function(response) {
+    console.log("I got the data I requested");
+    $scope.todolist = response;
+    $scope.todo = "";
+  });
+};
+
+refresh();
+
+$scope.addItem = function() {
+  console.log($scope.todo);
+  $http.post('/todolist', $scope.todo).success(function(response) {
+    console.log(response);
+    refresh();
+  });
+};
+
+$scope.remove = function(id) {
+  console.log(id);
+  $http.delete('/todolist/' + id).success(function(response) {
+    refresh();
+  });
+};
+
+$scope.edit = function(id) {
+  console.log(id);
+  $http.get('/todolist/' + id).success(function(response) {
+    $scope.todo = response;
+  });
+};  
+
+$scope.finish = function(id) {
+  console.log(id + " finish function");
+  $http.post('/todolist/' + id).success(function(response) {
+    refresh();
+  })
+  console.log("finish done");
+};
+
+$scope.update = function() {
+  console.log($scope.todo._id);
+  $http.put('/todolist/' + $scope.todo._id, $scope.todo).success(function(response) {
+    refresh();
+  })
+};
+
+$scope.deselect = function() {
+  $scope.todo = "";
+}
+
+
+}]);﻿
